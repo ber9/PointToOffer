d0b64c9fb6195cd6f16561d9c812fa44e7bb43da
berg1995 <wanyanwenbo@gmail.com>
2019-02-26 14:30:20 +0800

    problem9

--- src/Problems.java
+++ src/Problems.java
@@ -190,22 +190,29 @@ public class Problems {
      * Problem8:
      * 一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
      * <p>
-     * 解法：动态规划，f(1)=1，f(2)=2。f(n) = f(n-1) + f(n-2)
+     * 解法：设第一次跳了3阶，则剩下的可以表示为从第三阶开始跳的次数，f(n-3)。
+     * 以此类推，第一次跳了n次时，f(n) = f(n-1)+f(n-2)+···+f(n-n) = f(0)+f(1)+···+f(n-1)
+     * f(n-1) = f(0)+f(1)+···+f((n-1)-1)
+     * 上下相减得f(n) = 2f(n-1)
      */
     static int JumpFloorII(int target) {
         if (target <= 0)
-            return -1;
+            return 1;
         else if (target == 1)
             return 1;
-        else if (target == 2)
-            return 2;
         else
-            return jumpFloor(target - 1) + jumpFloor(target - 2);
+            return JumpFloorII(target - 1)*2;
 
     }
 
+    /**
+     * Problem9
+     *
+     */
+
+
     public static void main(String[] args) {
-        System.out.println(jumpFloor(5));
+        System.out.println(JumpFloorII(5));
 
     }
 }
